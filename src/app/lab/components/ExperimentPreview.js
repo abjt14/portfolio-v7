6889cd9c3021e18bd35e88852d61d4051fbbfe89
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2025-06-05 20:51:17 +0530

    adding family otp input

--- src/app/lab/components/ExperimentPreview.js
+++ src/app/lab/components/ExperimentPreview.js
@@ -47,7 +47,7 @@ export default function ExperimentPreview({ experiments }) {
             }}
           />
           <div
-            className="absolute top-0 left-0 h-full w-full bg-no-repeat bg-cover blur-lg z-10"
+            className="absolute top-0 left-0 h-full w-full bg-no-repeat bg-cover blur-sm z-10"
             style={{
               backgroundImage: `url(${placeholder})`,
             }}
